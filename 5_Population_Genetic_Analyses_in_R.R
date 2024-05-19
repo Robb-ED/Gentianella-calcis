68513bafb9f379d31d19ed00eccb834711b0d640
Robb ED <robbthecactuslover@gmail.com>
2024-05-20 10:54:49 +1200

    Added R analysis scripts

---
+++ 5_Population_Genetic_Analyses_in_R.R
@@ -0,0 +1,143 @@
+#Setwd, load libraries
+setwd("~/Gentianella_publication/final_run/")
+
+#Libraries
+library(ggplot2)
+library(vcfR)
+library(adegenet)
+library(StAMPP)
+library(dartR)
+
+#Colour palette
+calcis_colours <- c("#EAB64D","#AFC494","#0BB24E","#5c6f9d","deepskyblue","#DA8051","#857E61"
+                    ,"#90C8AD","orchid4","yellowgreen","#C97E8C","#CBD4C2")
+#read in VCF file
+calcis <- read.vcfR("noreps_maf_maxhet_noparas_randomSNP.vcf")
+
+#read in pop data - this will allow us to see if groups match up with a prior expectations
+pop.data <- read.table("popmap_noreps.txt", sep = "\t", header = T)
+#Should have two columns (AccessID and Population)
+
+#Create Genlight from the vcfR object and add information to it
+c_genlight <- vcfR2genlight(calcis)
+pop(c_genlight) <- pop.data$Population
+
+####First Analysis: PCA###
+#PCA using adgenet and ggplot2
+c_PCA <- glPca(c_genlight, center = TRUE, scale = TRUE, nf = 5, loadings = TRUE,
+               alleleAsUnit = FALSE, useC = TRUE, parallel = FALSE,
+               n.cores = NULL, returnDotProd=FALSE, matDotProd=NULL)
+
+barplot(100*c_PCA$eig/sum(c_PCA$eig), col = heat.colors(90), main="PCA Eigenvalues")
+title(ylab="Percent of variance\nexplained", line = 2)
+title(xlab="Eigenvalues", line = 1)
+
+#Calculate variation explained by axes
+eig <- round((100*c_PCA$eig/sum(c_PCA$eig)), digits = 1)
+
+#Extract PCA scores and merge with the population data
+pca_scores <- as.data.frame(c_PCA$scores)
+pca_scores["AccessID"] <- rownames(pca_scores)
+row.names(pca_scores) <- NULL
+pca_scores <- merge(pca_scores, pop.data, by ="AccessID")
+
+#PLOT the PCA - the sprintf function allows us to use a variable in a string 
+
+PCA_axes1_2 <- ggplot(pca_scores, aes(x=PC1, y = PC2)) + 
+  geom_hline(yintercept = 0, color = "grey", linewidth=0.5, linetype = 2) + 
+  geom_vline(xintercept = 0, color = "grey", linewidth =0.5, linetype = 2) +
+  geom_point(aes(color=Population, shape = Taxon), size =2) +
+  stat_ellipse(aes(group = Population, color = Population)) +
+  xlab(sprintf("PC1 %s %%", eig[1])) +
+  ylab(sprintf("PC2 %s %%", eig[2])) +
+  ggtitle("PCA axes 1 & 2") +
+  scale_colour_manual(values = calcis_colours) +
+  theme_classic() +
+  theme(legend.position = "none") 
+
+
+PCA_axes3_4 <- ggplot(pca_scores, aes(x=PC3, y = PC4)) + 
+  geom_hline(yintercept = 0, color = "grey", linewidth=0.5, linetype = 2) + 
+  geom_vline(xintercept = 0, color = "grey", linewidth =0.5, linetype = 2) +
+  geom_point(aes(color=Population, shape = Taxon), size =2) +
+  stat_ellipse(aes(group = Population, color = Population)) +
+  xlab(sprintf("PC3 %s %%", eig[3])) +
+  ylab(sprintf("PC4 %s %%", eig[4])) +
+  ggtitle("PCA axes 3 & 4") +
+  scale_colour_manual(values = calcis_colours) +
+  theme_classic() +
+  theme(legend.position = "right")
+
+PCA_axes1_2 + PCA_axes3_4
+
+###Write a .phylip file for splitstree###
+
+#Create a matrix of pairwise Nei's distances between samples
+gen_dist <- stamppNeisD(c_genlight, pop = FALSE)
+
+#Write this out as a .phy file to the working directory
+stamppPhylip(gen_dist, file = "input_for_splitstree")
+
+###AMOVA###
+#Libraries
+library(ade4)
+library(poppr)
+
+#Add the AMOVA strata file
+strata(c_genlight) <- pop.data[2]
+nameStrata(c_genlight) <- ~Population
+
+#Perform an AMOVA that tests at the population level
+amova_obj <- poppr.amova(c_genlight, 
+                         hier = ~Population,
+                         threads = 3,
+                         method = "ade4",
+                         within = FALSE)
+
+#Check your results
+amova_obj
+amova_obj$varcomp/sum(amova_obj$varcomp)
+
+#Test if pops significantly different using randomization test
+set.seed(2847)
+c_sig <- randtest(amova_obj, nrepet = 10000)
+
+
+#Visualise the results - black line shouldn't fall within observed 
+plot(c_sig)
+c_sig
+
+write.table(amova_obj$componentsofcovariance, file = "AMOVA_covariance.txt", quote = FALSE, sep = "\t")
+
+###Pairwise Fst###
+#Fst with P values from the StAMMP package
+pvaluefst <- stamppFst(c_genlight, nboots = 50000, percent = 95.000, nclusters = 3)
+#If you use too few bootstraps, your p values will be small (i.e., 2dp)
+fstmat <- pvaluefst$Fsts
+
+#even with over 100,000 boostraps we didn't get P values of greater than 0
+# based on trials using less differentiated groups, we can conclude everything has a P value of at least ~0.00001
+pvals <- pvaluefst$Pvalues
+pvals <- format((pvals[1-12,1-12]+0.00001), scientific = FALSE)
+
+#apply BF correction for multiple comparisons
+adjusted <- p.adjust(pvals, method = "bonferroni")
+adjusted
+
+write.table(fstmat, file = "Pairwise_Fst.txt", quote = FALSE, sep = "\t")
+
+
+###IBD###
+#Input a new popmap consisting of the sample names, population, taxon, and sampling coords (lat lon format)
+
+popmap_samplingcoords <- read.table("popmap_and_sampcoords.txt", sep = "\t", header = T)
+
+#Separate out only the lat lon 
+latlon <- popmap_samplingcoords[,4:5]
+
+#Perform IBD test using euclidean distances
+ibd_result <- gl.ibd(c_genlight,
+                     distance = 'euclidean',
+                     coordinates = latlon,
+                     permutations = 99999)
+
---
+++ 7_Putative_Outlier_Detection_and_Plotting.R
@@ -0,0 +1,214 @@
+#####Outlier Detection######
+#Following this tutorial https://bcm-uga.github.io/pcadapt/articles/pcadapt.html
+#Set working directory, read in data
+setwd("~/Gentianella_publication/final_run/")
+library(vcfR)
+library(adegenet)
+library(dartR)
+
+#colours
+calcis_colours <- c("#EAB64D","#AFC494","#0BB24E","#5c6f9d","deepskyblue","#DA8051","#857E61"
+                    ,"#90C8AD","orchid4","yellowgreen","#C97E8C","#CBD4C2")
+
+##Read the files
+ecology <- read.vcfR("noreps_lessthanmean.vcf")
+
+#Population data
+pop.data <- read.table("popmap_morethanmean.txt", sep = "\t", header = TRUE)
+
+v_genlight <- vcfR2genlight(ecology)
+pop(v_genlight) <- pop.data$Population
+
+
+##Using PCAdapt####
+library(dartR)
+library(pcadapt)
+
+#Set the path to the VCF file
+path <- "~/Gentianella_publication/final_run/noreps_lessthanmean.vcf"
+
+#Read in the file
+v_adapt <- read.pcadapt(path, type = ("vcf"))
+
+##Perform the pcadapt - specify a number of axes to retain initially
+pc_v <- pcadapt(input=v_adapt,K=20)
+
+#Check the axes using a screeplot. We are looking for where the points deviate from the straight
+#Line (i.e., Cattell's rule)
+plot(pc_v, option ="screeplot")
+#Based on this it looks like K=10 is most appropriate
+
+#Repeat with K=10
+pc_v <- pcadapt(input=v_adapt,K=10)
+plot(pc_v,option="manhattan")
+plot(pc_v, option = "qqplot")
+
+#Plot the p values, those with really small ones are outliers
+hist(pc_v$pvalues, xlab = "p-values", main = "Frequency of p-values", breaks = 50, col = "orchid3")
+
+#Add the P values to their own dataframe, rename the column
+pval <- as.data.frame(pc_v$pvalues)
+colnames(pval)[1] <- "p_value"
+#Add the SNP number to the next column (assumes the pvalues kept the order of the SNPS)
+pval$SNP <- v_genlight$loc.names
+#Perform bonferroni method which is a conservative way of finding SNP outliers
+pval$bf <- p.adjust(pval$p_value, method="bonferroni")
+#add 1s to identify which SNPs are outliers and which aren't (0)
+pval$outlier <- ifelse(pval$bf < 0.1, '1',
+                       ifelse(pval$p_value > 0.1, '0', '0'))
+#Check how many there are
+length(which(pval$outlier==1))
+
+#Separate the Two dataframes
+SNP_ouliers <- pval[which(pval$outlier==1),]
+SNP_normals <- pval[which(pval$outlier==0),]
+
+#Create a new GL using dartR that contains only the outlier loci
+gl_outliers <- gl.keep.loc(v_genlight, SNP_ouliers$SNP)
+
+
+# Create two PCA for the outlier dataset 
+o_PCA <- glPca(gl_outliers, center = TRUE, scale = TRUE, nf = 8, loadings = TRUE,
+               alleleAsUnit = FALSE, useC = TRUE, parallel = FALSE,
+               n.cores = NULL, returnDotProd=FALSE, matDotProd=NULL)
+
+#Variation explained by axes
+eig <- round((100*o_PCA$eig/sum(o_PCA$eig)), digits = 1)
+
+#Extract PCA scores and merge with the population data
+pca_scores <- as.data.frame(o_PCA$scores)
+pca_scores["AccessID"] <- rownames(pca_scores)
+row.names(pca_scores) <- NULL
+pca_scores <- merge(pca_scores, pop.data, by ="AccessID")
+
+#PLOT - the sprintf function allows us to use a variable in a string 
+outlierPCA1_2 <- ggplot(pca_scores, aes(x=PC1, y = PC2)) + 
+  geom_hline(yintercept = 0, color = "grey", linewidth=0.5, linetype = 2) + 
+  geom_vline(xintercept = 0, color = "grey", linewidth =0.5, linetype = 2) +
+  geom_point(aes(color=Population, shape=Taxon), size =2) +
+  stat_ellipse(aes(group = Population, color = Population)) +
+  xlab(sprintf("PC1 %s %%", eig[1])) +
+  ylab(sprintf("PC2 %s %%", eig[2])) +
+  ggtitle("PCA of outliers axes 1 & 2") +
+  scale_colour_manual(values=calcis_colours) +
+  theme_classic() + theme(legend.position = "none")
+  
+  
+
+outlierPCA3_4 <- ggplot(pca_scores, aes(x=PC3, y = PC4)) + 
+  geom_hline(yintercept = 0, color = "grey", linewidth=0.5, linetype = 2) + 
+  geom_vline(xintercept = 0, color = "grey", linewidth =0.5, linetype = 2) +
+  geom_point(aes(color=Population, shape=Taxon), size =2) +
+  stat_ellipse(aes(group = Population, color = Population)) +
+  xlab(sprintf("PC3 %s %%", eig[3])) +
+  ylab(sprintf("PC4 %s %%", eig[4])) +
+  ggtitle("PCA of outliers axes 3 & 4") +
+  scale_colour_manual(values=calcis_colours) +
+  theme_classic() + theme(legend.position = "right") + scale_shape(guide = FALSE) 
+  
+  
+
+##Repeat this but with neutral loci
+gl_normals <- gl.keep.loc(v_genlight, SNP_normals$SNP)
+##Now run it back through the PCA code 
+n_PCA <- glPca(gl_normals, center = TRUE, scale = FALSE, nf = 5, loadings = TRUE,
+               alleleAsUnit = FALSE, useC = TRUE, parallel = FALSE,
+               n.cores = NULL, returnDotProd=FALSE, matDotProd=NULL)
+
+
+#Variation explained by axes
+eig <- round((100*n_PCA$eig/sum(n_PCA$eig)), digits = 1)
+
+#Extract PCA scores and merge with the population data
+pca_scores <- as.data.frame(n_PCA$scores)
+pca_scores["AccessID"] <- rownames(pca_scores)
+row.names(pca_scores) <- NULL
+pca_scores <- merge(pca_scores, pop.data, by ="AccessID")
+
+#PLOT - the sprintf function allows us to use a variable in a string 
+normalPCA1_2 <- ggplot(pca_scores, aes(x=PC1, y = PC2)) + 
+  geom_hline(yintercept = 0, color = "grey", linewidth=0.5, linetype = 2) + 
+  geom_vline(xintercept = 0, color = "grey", linewidth =0.5, linetype = 2) +
+  geom_point(aes(color=Population, shape = Taxon), size =2) +
+  stat_ellipse(aes(group = Population, color = Population)) +
+  xlab(sprintf("PC1 %s %%", eig[1])) +
+  ylab(sprintf("PC2 %s %%", eig[2])) +
+  ggtitle("PCA of neutral SNPS axes 1 & 2") +
+  scale_colour_manual(values = calcis_colours) +
+  theme_classic() +
+  theme(legend.position = "none") 
+  
+
+
+normalPCA3_4 <- ggplot(pca_scores, aes(x=PC3, y = PC4)) + 
+  geom_hline(yintercept = 0, color = "grey", linewidth=0.5, linetype = 2) + 
+  geom_vline(xintercept = 0, color = "grey", linewidth =0.5, linetype = 2) +
+  geom_point(aes(color=Population, shape = Taxon), size =2) +
+  stat_ellipse(aes(group = Population, color = Population)) +
+  xlab(sprintf("PC3 %s %%", eig[3])) +
+  ylab(sprintf("PC4 %s %%", eig[4])) +
+  ggtitle("PCA of neutral SNPS axes 3 & 4") +
+  scale_colour_manual(values = calcis_colours, guide = FALSE) +
+  theme_classic() +
+  theme(legend.position = "right")
+  
+
+combined <- outlierPCA1_2+outlierPCA3_4+normalPCA1_2+normalPCA3_4 
+
+combined 
+
+## Investigate potential for overdominance
+outlier_matrix <- as.matrix(gl_outliers) 
+normal_matrix <- as.matrix(gl_normals)
+
+# Recode genotypes and add pop data
+outlier_fre <- data.frame(row.names = gl_outliers$ind.names)
+outlier_fre$ref <- rowSums(outlier_matrix == '0', na.rm = TRUE)
+outlier_fre$alt <- rowSums(outlier_matrix == '2', na.rm = TRUE)
+outlier_fre$het <- rowSums(outlier_matrix == '1', na.rm = TRUE)
+outlier_fre$pops <- pop.data$Population
+
+outlier_avgs <- aggregate(ref ~ pops, data = outlier_fre, mean)
+outlier_avgs$alt <- (aggregate(alt ~ pops, data = outlier_fre, mean))$alt
+outlier_avgs$het <- (aggregate(het ~ pops, data = outlier_fre, mean))$het
+
+outlier_avgs <- t(outlier_avgs)
+colnames(outlier_avgs) <- outlier_avgs[1,]
+outlier_avgs <- outlier_avgs[-1,]
+
+# Plot
+par(mar = c(8, 4.1, 4.1, 7))
+barplot(outlier_avgs, las = 2, cex.names = 0.75,
+        col = c("orchid2","orchid3", "orchid4"),
+        main = "Average Allele Frequencies in Outlier Dataset",
+        ylab = "Proportion of Alleles",
+        axes = TRUE,
+        legend.text = rownames(outlier_avgs),
+        args.legend = list(x = "topright",
+                           inset = c(-0.2, 0)))
+
+# Recode genotypes 
+normal_fre <- data.frame(row.names = gl_outliers$ind.names)
+normal_fre$ref <- rowSums(normal_matrix == '0', na.rm = TRUE)
+normal_fre$alt <- rowSums(normal_matrix == '2', na.rm = TRUE)
+normal_fre$het <- rowSums(normal_matrix == '1', na.rm = TRUE)
+normal_fre$pops <- pop.data$Population
+
+normal_avgs <- aggregate(ref ~ pops, data = normal_fre, mean)
+normal_avgs$alt <- (aggregate(alt ~ pops, data = normal_fre, mean))$alt
+normal_avgs$het <- (aggregate(het ~ pops, data = normal_fre, mean))$het
+
+normal_avgs <- t(normal_avgs)
+colnames(normal_avgs) <- normal_avgs[1,]
+normal_avgs <- normal_avgs[-1,]
+
+# Plot
+par(mar = c(8, 4.1, 4.1, 7))
+barplot(normal_avgs, las = 2, cex.names = 0.75,
+        col = c("orchid2","orchid3", "orchid4"),
+        main = "Average Allele Frequencies in Neutral Dataset",
+        ylab = "Proportion of Alleles",
+        axes = TRUE,
+        legend.text = rownames(outlier_avgs),
+        args.legend = list(x = "topright",
+                           inset = c(-0.2, 0)))
